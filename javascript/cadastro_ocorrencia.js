4b47da6b252241f0274d6d1c46da5fa76144d29f
Pedro Barcellos <pedrobobmv@hotmail.com>
2019-05-31 10:31:33 -0300

    Criar fetch para Cadastro Ocorrência.

---
+++ javascript/cadastro_ocorrencia.js
@@ -0,0 +1,47 @@
+const btCadastro = document.querySelector('#btCadastrar');
+
+const teste = document.querySelectorAll('#form-cadastro input, select');
+  
+const gerarDadosForm = campos => {
+    let dados = {};
+    
+    for(let campo of campos){
+      dados[campo.name] = campo.value;
+    }
+  
+    return dados;
+}
+
+const cadastrarOcorrencia = (event) => {
+    event.preventDefault();
+    const campos = document.querySelectorAll('#form-cadastro input, select');
+    const dados = gerarDadosForm(campos);
+    dados.resolvido =false;
+    dados.coordenadas = {
+        latitude: dados.latitude,
+        longitude: dados.longitude
+    };
+
+    delete dados.latitude;
+    delete dados.longitude;
+
+    console.log(dados);
+
+    fetch('http://104.46.38.224/api/ocorrencia/cadastrar', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(dados)
+    })
+    .then(resposta => resposta.json())
+    .then(respostaJson => {
+      alert('Ocorrência cadastrada com sucesso!');
+      console.log(dados);
+      //window.location = ""
+    });
+}
+
+if(btCadastro){
+    btCadastro.onclick = cadastrarOcorrencia;
+}
--- javascript/ocorrencias_geral.js
+++
@@ -1,7 +0,0 @@
-function sair() {
-    let sair = confirm("Tem certeza que deseja sair ?");
-    if(sair==true){
-        location.href = "../html/login.html";
-    }
-}
-
